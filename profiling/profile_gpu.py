ded0e10a9332db665979d4ae1dfdc027af1a159e
ReiGaan <107040444+ReiGaan@users.noreply.github.com>
2025-07-12 12:27:27 +0200

    add metal backend for mac users

--- profiling/profile_gpu.py
+++ profiling/profile_gpu.py
@@ -4,7 +4,20 @@ from agents.alphazero.network import Connect4Net
 
 # Initialize model and dummy input
 model = Connect4Net()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Detect device
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    activities = [ProfilerActivity.CPU, ProfilerActivity.CUDA]
+    profile_dir = "./cuda_profile_logs"
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+    activities = [ProfilerActivity.CPU, ProfilerActivity.MPS]
+    profile_dir = "./mps_profile_logs"
+else:
+    device = torch.device("cpu")
+    activities = [ProfilerActivity.CPU]
+    profile_dir = "./cpu_profile_logs"
+
 model.to(device)
 
 # Adjust the input size based on the model's requirements
@@ -12,8 +25,8 @@ dummy_input = torch.randn(1, 3, 6, 7).to(device)  # Batch size 1, 3 channels, 6x
 
 # GPU profiling
 with profile(
-    activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-    on_trace_ready=torch.profiler.tensorboard_trace_handler('./gpu_profile_logs'),
+    activities=activities,
+    on_trace_ready=torch.profiler.tensorboard_trace_handler(profile_dir),
     record_shapes=True,
     with_stack=True
 ) as prof:
@@ -21,5 +34,6 @@ with profile(
         output = model(dummy_input)
 
 # Write profiling results to a file
-with open('gpu_profile_results.log', 'w') as log_file:
-    log_file.write(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+log_file_path = f"{device}_profile_results.log"
+with open(log_file_path, 'w') as log_file:
+    log_file.write(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
